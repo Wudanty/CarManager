e4d888809d6cbb0979a13175881abe0c810a96fc
Arkadiusz Mańczyk <aro03211@wp.pl>
2022-11-21 15:16:34 +0100

    Add Db class, modify Car and CarTodo

--- app/src/main/java/com/example/carmanager/models/CarTodo.java
+++ app/src/main/java/com/example/carmanager/models/CarTodo.java
@@ -1,9 +1,11 @@
 package com.example.carmanager.models;
 
-import lombok.Data;
 
-@Data
+import java.util.ArrayList;
+
 public class CarTodo {
+    public static ArrayList<CarTodo> listOfCarTodo = new ArrayList<>();
+
     private Integer carTodoId;
     private Integer carId;
     private String name;
@@ -13,6 +15,38 @@ public class CarTodo {
         this.carTodoId = carTodoId;
         this.carId = carId;
         this.name = name;
+        this.description = description;
+    }
+
+    public Integer getCarTodoId() {
+        return carTodoId;
+    }
+
+    public void setCarTodoId(Integer carTodoId) {
+        this.carTodoId = carTodoId;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 }
