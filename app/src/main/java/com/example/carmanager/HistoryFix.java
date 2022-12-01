aae4e658ee1d0d4908aee37de9edd9f396920ebe
Arkadiusz Mańczyk <aro03211@wp.pl>
2022-12-01 15:29:58 +0100

    Add history views

---
+++ app/src/main/java/com/example/carmanager/HistoryFix.java
@@ -0,0 +1,14 @@
+package com.example.carmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HistoryFix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history_fix);
+    }
+}
