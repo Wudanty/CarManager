6acc00765606460fefef08835e7f9f2a0af13eb5
Adrian00pl <gwiazdowskiadrian000@wp.pl>
2022-12-04 20:25:24 +0100

    add database connection

--- app/src/main/java/com/example/carmanager/MainActivity.java
+++ app/src/main/java/com/example/carmanager/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.carmanager.models.Car;
 import com.example.carmanager.models.Maintenance;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -32,11 +33,13 @@ public class MainActivity extends AppCompatActivity {
     Button btnCar, btnMoreActivities, btnHistory, btnSettings, btnMainActivity,btnDataCar,btnDetailsCar,btnStatisticsCar;
     LinearLayout dataLayout, detailsLayout, statisticsLayout;
     FloatingActionButton floatingButton;
+    TextView text_nazwa,text_marka,text_model,text_tablica,text_rok,text_polisa,text_vin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        DbManager dbManager = DbManager.instanceOfDatabase(this);
+        dbManager.fillCarArrayList();
         //Toolbar-----------------------------------------------
         btnCar = findViewById(R.id.car);
         btnMoreActivities = findViewById(R.id.more);
@@ -50,10 +53,24 @@ public class MainActivity extends AppCompatActivity {
         detailsLayout = findViewById(R.id.ll_details);
         statisticsLayout = findViewById(R.id.ll_statistics);
         floatingButton = findViewById(R.id.fab);
+        Car object = Car.listOfCars.get(1);
+        text_nazwa = findViewById(R.id.nazwa_v);
+        text_marka = findViewById(R.id.marka_v);
+        text_model = findViewById(R.id.model_v);
+        text_tablica = findViewById(R.id.tablica_v);
+        text_rok = findViewById(R.id.rok_v);
+        text_polisa = findViewById(R.id.polisa_v);
+        text_vin = findViewById(R.id.vin_v);
+        text_nazwa.setText(object.getCarNickname());
+        text_marka.setText(object.getBrand());
+        text_model.setText(object.getModel());
+        text_tablica.setText(object.getRegistry());
+        text_rok.setText(object.getProductionDate().toString());
+        text_polisa.setText(object.getTankVolume().toString());
+        text_vin.setText(object.getVin());
         btnDataCar.setBackgroundColor(btnDataCar.getContext().getResources().getColor(R.color.purple_700));
         btnDetailsCar.setBackgroundColor(btnDetailsCar.getContext().getResources().getColor(R.color.purple_500));
         btnStatisticsCar.setBackgroundColor(btnStatisticsCar.getContext().getResources().getColor(R.color.purple_500));
-        dataLayout.setVisibility(View.VISIBLE);
         detailsLayout.setVisibility(View.GONE);
         statisticsLayout.setVisibility(View.GONE);
 
