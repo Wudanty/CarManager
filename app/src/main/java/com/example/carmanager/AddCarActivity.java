45f796789669d2666b236d2bb474a555877af800
Wudanty <youheixd@gmail.com>
2022-12-10 13:13:53 +0100

    add activity for adding cars

---
+++ app/src/main/java/com/example/carmanager/AddCarActivity.java
@@ -0,0 +1,86 @@
+package com.example.carmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Objects;
+
+public class AddCarActivity extends AppCompatActivity {
+
+    //Toolbar-----------------------------------------------
+    public androidx.appcompat.widget.Toolbar toolbar;
+    Button btnCar, btnMoreActivities, btnHistory, btnSettings, btnMainActivity;
+    //Toolbar-----------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_car);
+
+        //Toolbar-----------------------------------------------
+        btnCar = findViewById(R.id.car);
+        btnMoreActivities = findViewById(R.id.more);
+        btnSettings = findViewById(R.id.settings);
+        btnMainActivity = findViewById(R.id.mainActivity);
+        btnHistory = findViewById(R.id.history);
+        toolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(null);
+
+
+
+
+
+
+
+
+
+
+        btnCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSP = new Intent(AddCarActivity.this, CarActivity.class);
+                intentSP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity( intentSP );
+            }
+        });
+        btnMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSP = new Intent(AddCarActivity.this, MainActivity.class);
+                intentSP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity( intentSP );
+            }
+        });
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSP = new Intent(AddCarActivity.this, History.class);
+                intentSP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity( intentSP );
+            }
+        });
+        btnMoreActivities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSP = new Intent(AddCarActivity.this, MoreActivities.class);
+                intentSP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity( intentSP );
+            }
+        });
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSP = new Intent(AddCarActivity.this, Settings.class);
+                intentSP.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity( intentSP );
+            }
+        });
+        //Toolbar-----------------------------------------------
+
+    }
+}
