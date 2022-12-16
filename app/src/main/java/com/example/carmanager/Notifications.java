9ce57df4081e9b07fe94b5a85d695b6a2a81e93e
Adrian00pl <gwiazdowskiadrian000@wp.pl>
2022-12-16 18:34:07 +0100

    add alarmMenager

--- app/src/main/java/com/example/carmanager/Notifications.java
+++ app/src/main/java/com/example/carmanager/Notifications.java
@@ -1,17 +1,15 @@
 package com.example.carmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,12 +21,11 @@ import android.widget.ListView;
 import android.widget.Spinner;
 
 import com.example.carmanager.models.Car;
+import com.example.carmanager.models.Mileage;
 import com.example.carmanager.models.Notification;
 import com.example.carmanager.models.NotificationReceiver;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 public class Notifications extends AppCompatActivity {
     DbManager dbManager = DbManager.instanceOfDatabase(this);
@@ -45,7 +42,7 @@ public class Notifications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
-        createAlarm();
+        createNotificationChannel();
         notification_list = findViewById(R.id.list_not);
         btnCar = findViewById(R.id.car);
         btnMoreActivities = findViewById(R.id.more);
@@ -53,16 +50,16 @@ public class Notifications extends AppCompatActivity {
         btnHistory = findViewById(R.id.history);
         checkBoxKm = findViewById(R.id.checkBox);
         checkBoxDate = findViewById(R.id.checkBox2);
-        editTextKm = findViewById(R.id.editTextNumber);
-        editTextDate = findViewById(R.id.editTextDate);
+        editTextKm = findViewById(R.id.editKm);
+        editTextDate = findViewById(R.id.editDate);
         editTextDes = findViewById(R.id.editTextTextMultiLine);
         spino = findViewById(R.id.spinner);
         selectCarSpinner = findViewById(R.id.spinner2);
         layout_add = findViewById(R.id.layout_add);
         layout_list = findViewById(R.id.layout_list);
-        button_add_a = findViewById(R.id.button_add_a);
+        button_add_a = findViewById(R.id.button_data);
         button_add = findViewById(R.id.button_add);
-        button_list = findViewById(R.id.button_list);
+        button_list = findViewById(R.id.button_details);
         button_add_a.setBackgroundColor(button_add_a.getContext().getResources().getColor(R.color.purple_700));
         button_list.setBackgroundColor(button_list.getContext().getResources().getColor(R.color.purple_500));
         ArrayAdapter ad = new ArrayAdapter(this,android.R.layout.simple_spinner_item, option);
@@ -105,8 +102,22 @@ public class Notifications extends AppCompatActivity {
         button_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               //Notification notification = new Notification(car.getCarId(),"12.12.12",editTextDes.getText().toString(),1,1);
-               //dbManager.addNotificationToDb(notification);
+               /* int typ = 0;
+                Double kilometry= 0.0;
+                String data=null;
+                if (checkBoxDate.isChecked()){
+                    typ=1;
+                    data =editTextDate.getText().toString();
+                }
+                else if (checkBoxKm.isChecked()){
+                    dbManager.getCarHistory(car.getCarId());
+                    typ=0;
+                    kilometry = Mileage.listOfMIleage.get(Mileage.listOfMIleage.size() - 1).getMileageValue()+Double.parseDouble(editTextKm.getText().toString());
+                }
+
+              Notification notification = new Notification(car.getCarId(),data,editTextDes.getText().toString(),null,typ,kilometry.intValue(),spino.getSelectedItem().toString());
+              dbManager.addNotificationToDb(notification);*/
+              createAlarm();
             }
         });
         btnCar.setOnClickListener(new View.OnClickListener() {
@@ -146,22 +157,41 @@ public class Notifications extends AppCompatActivity {
     private void createAlarm() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.HOUR_OF_DAY, 15);
+        calendar.set(Calendar.MINUTE, 45);
         Intent intent = new Intent(this, NotificationReceiver.class);
         AlarmManager alarmMgr = (AlarmManager) this.getSystemService(ALARM_SERVICE);
-        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_NO_CREATE);
-        if (alarmMgr != null) {
-            alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
     }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "TestName";
+            String description = "TestDes";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     public void itemClicked(View v) {
         CheckBox checkBoxDate = (CheckBox)v;
         if(checkBoxDate.isChecked()){
             editTextDate.setVisibility(View.VISIBLE);
+            checkBoxKm.setChecked(false);
+            editTextKm.setVisibility(View.GONE);
         }
         else {
             editTextDate.setVisibility(View.GONE);
+            checkBoxKm.setChecked(true);
+            editTextKm.setVisibility(View.VISIBLE);
         }
 
     }
@@ -169,9 +199,13 @@ public class Notifications extends AppCompatActivity {
         CheckBox checkBoxKm = (CheckBox)v;
         if(checkBoxKm.isChecked()){
             editTextKm.setVisibility(View.VISIBLE);
+            checkBoxDate.setChecked(false);
+            editTextDate.setVisibility(View.GONE);
         }
         else {
             editTextKm.setVisibility(View.GONE);
+            checkBoxDate.setChecked(true);
+            editTextDate.setVisibility(View.VISIBLE);
         }
 
     }
