d9d71c1d2567d05ac88f5f878fa051b4ce6f59e6
Szymaniak <szymanski.damian.1999@gmail.com>
2023-01-22 14:32:16 +0100

    update insurance3

--- app/src/main/java/com/example/carmanager/AdapterInsurance.java
+++ app/src/main/java/com/example/carmanager/AdapterInsurance.java
@@ -1,4 +1,46 @@
 package com.example.carmanager;
 
-public class AdapterInsurance {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.carmanager.models.Fix;
+import com.example.carmanager.models.Insurance;
+
+import java.util.List;
+
+public class AdapterInsurance extends ArrayAdapter<Insurance> {
+    public AdapterInsurance(Context context, List<Insurance> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fix_cell, parent, false);
+
+        TextView odKiedy = convertView.findViewById(R.id.textViewOdKiedy);
+        TextView doKiedy = convertView.findViewById(R.id.textViewDoKiedy);
+        TextView cena = convertView.findViewById(R.id.textViewCenaUbezpieczenia);
+        TextView ubezpieczyciel = convertView.findViewById(R.id.textViewUbezpieczyciel);
+        TextView rodzaj = convertView.findViewById(R.id.textViewRodzaj);
+        TextView numer = convertView.findViewById(R.id.textViewNumerPolisy);
+
+        odKiedy.setText(getItem(position).getStartDate()+"");
+        doKiedy.setText(getItem(position).getExpirationDate()+"");
+        cena.setText(getItem(position).getPrice()+"");
+        ubezpieczyciel.setText(getItem(position).getProvider()+"");
+        rodzaj.setText(getItem(position).getInsuranceType()+"");
+        numer.setText(getItem(position).getInsuranceNumber()+"");
+
+        return convertView;
+    }
 }
